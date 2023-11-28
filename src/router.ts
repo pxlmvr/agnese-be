8de999844ddfca9b66b1c0bb46a14fcdc22b8e32
pxlmvr <matteo.losi@casavo.com>
2023-11-28 14:44:39 +0100

    wip: stub routes

---
+++ src/router.ts
@@ -0,0 +1,27 @@
+import { Router } from 'express'
+
+// Router is not a constructor function, still it is uppercased because of old weird conventions
+const router = Router()
+
+/* Recipes */
+router.get('/recipes', () => {})
+router.get('/recipes/:id', () => {})
+router.put('/recipes/:id', () => {})
+router.post('/recipes', () => {})
+router.delete('/recipes/:id', () => {})
+
+/* Ingredients */
+router.get('/ingredients', () => {})
+// router.get('/ingredient/id', () => {})
+router.put('/ingredients/:id', () => {})
+router.post('/ingredients', () => {})
+router.delete('/ingredients/:id', () => {})
+
+/* Categories */
+router.get('/categories', () => {})
+// router.get('/categories/:id', () => {})
+router.put('/categories/:id', () => {})
+router.post('/categories', () => {})
+router.delete('/categories/:id', () => {})
+
+export default router
